1d861480fd3e0df6a04570b53bb21f6a15c71bd0
Jose Castaneyra <jcastaneyra@gmail.com>
2014-11-12 17:44:43 -0600

    Change api url to heroku one

--- SPA/app/scripts/services/api.js
+++ SPA/app/scripts/services/api.js
@@ -15,8 +15,8 @@ angular.module('spaApp').factory('api', ['$http', '$rootScope', function ($http,
       console.log("Executes init & token = " + $rootScope.session_token);
     },
     config: function(){
-      //$rootScope.restAPIBaseUrl = "http://abanking-api.herokuapp.com";
-      $rootScope.restAPIBaseUrl = "http://localhost:9001";
+      $rootScope.restAPIBaseUrl = "http://abanking-api.herokuapp.com";
+      //$rootScope.restAPIBaseUrl = "http://localhost:9001";
       $rootScope.publishKey = "pub-c-90b1707f-26e4-4c7a-bbe4-79e695283d79";
       $rootScope.subscribeKey = "sub-c-439ead5e-8935-11e3-baad-02ee2ddab7fe";
     }
